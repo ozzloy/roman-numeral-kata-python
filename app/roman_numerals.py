2c345752a70c11f721537195e70cc66665cbc5cc
daniel watson <ozzloy@each.do>
2024-12-06 20:42:59 -0800

    init repo

---
+++ app/roman_numerals.py
@@ -0,0 +1,5 @@
+def parse(roman):
+    if roman == "I":
+        return 1
+    if roman == "II":
+        return 2
---
+++ test/test_roman_numerals_unittest.py
@@ -0,0 +1,14 @@
+import unittest
+from app.roman_numerals import parse
+
+
+class TestRomanNumerals(unittest.TestCase):
+    def test_i(self):
+        value = parse("I")
+
+        self.assertEqual(value, 1)
+
+    def test_ii(self):
+        value = parse("II")
+
+        self.assertEqual(value, 2)
