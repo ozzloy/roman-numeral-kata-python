ef152459123ad75b9085a88d9560630ebbda4672
daniel watson <ozzloy@each.do>
2024-12-07 18:10:06 -0800

    evaluate chunks

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,3 +1,39 @@
+from functools import reduce
+
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+
+def chunk_once(chunks, single_roman):
+    if not chunks:
+        return [[single_roman, 1]]
+    last_chunk = chunks[-1]
+    if single_roman == last_chunk[0]:
+        last_chunk[1] += 1
+    else:
+        chunks.append([single_roman, 1])
+    return chunks
+
+
+def chunk(romans):
+    return reduce(chunk_once, romans, [])
+
+
+def evaluate_chunk(chunk):
+    return values[chunk[0]] * chunk[1]
+
+
+def evaluate_chunks(chunks):
+    return [1]
+
+
 def parse(roman):
     values = {
         "V": 5,
@@ -9,4 +45,6 @@ def parse(roman):
         if roman[-1] == "I":
             return len(roman)
         return values[roman[-1]] - (len(roman) - 1)
+    if roman[0] == "X":
+        return 10 + parse(roman[1:])
     return values[roman[0]] + (len(roman) - 1)
--- test/test_roman_numerals_pytest.py
+++ test/test_roman_numerals_pytest.py
@@ -1,5 +1,64 @@
-from app.roman_numerals import parse
+import pytest
+from app.roman_numerals import (
+    parse,
+    chunk,
+    chunk_once,
+    evaluate_chunk,
+    evaluate_chunks,
+)
+
+I = "I"
+V = "V"
 
 
-def test_roman_numeral_parser():
-    assert parse("IX") == 9
+@pytest.mark.parametrize(
+    "chunks,single_roman,expected",
+    [
+        ([], I, [[I, 1]]),
+        ([[I, 1]], I, [[I, 2]]),
+        ([[I, 1]], V, [[I, 1], [V, 1]]),
+    ],
+)
+def test_chunk_once(chunks, single_roman, expected):
+    assert chunk_once(chunks, single_roman) == expected
+
+
+@pytest.mark.parametrize(
+    "romans,expected",
+    [(I, [[I, 1]]), (I + I, [[I, 2]]), (I + V, [[I, 1], [V, 1]])],
+)
+def test_chunk(romans, expected):
+    assert chunk(romans) == expected
+
+
+@pytest.mark.parametrize(
+    "chunk,expected",
+    [([I, 1], 1), ([I, 2], 2), ([V, 1], 5), ([V, 2], 10)],
+)
+def test_evaluate_chunk(chunk, expected):
+    assert evaluate_chunk(chunk) == expected
+
+
+@pytest.mark.parametrize("chunks,expected", [([[I, 1]], [1])])
+def test_evaluate_chunks(chunks, expected):
+    assert evaluate_chunks(chunks) == expected
+
+
+@pytest.mark.parametrize(
+    "roman,expected",
+    [
+        ("IX", 9),
+        ("X", 10),
+        ("XI", 11),
+        ("XIV", 14),
+        ("XV", 15),
+        ("XVI", 16),
+        ("XIX", 19),
+        ("XX", 20),
+        ("XXV", 25),
+        ("XXXIV", 34),
+        # ("XLI", 41),
+    ],
+)
+def test_roman_numeral_parser(roman, expected):
+    assert parse(roman) == expected
